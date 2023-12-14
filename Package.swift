0edd7d6774ec66c0b922400a9aaf3b9e58abac34
Yannik Ehlert <yannik.ehlert@claas.com>
2023-12-14 08:04:06 +0100

    Rename library

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     products: [
         .executable(name: "xcc", targets: ["XcodeCoverageConverter"]),
-        .library(name: "XcodeCoverageConverter", targets: ["Core"]),
+        .library(name: "CoverageConverterCore", targets: ["Core"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
